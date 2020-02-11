afa37079ad618e4f81deaeb33f56daaab141c1d3
206620219 <Brian.DeMaio@universalorlando.com>
2020-02-11 16:18:58 -0500

    Added particle and updated it's position based on velocity

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+let p;
+
 function setup() {
   //Set canvas to window height and width
   createCanvas(window.innerWidth, window.innerHeight);
+  p = new Particle();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(100);
+  background("blue");
+  p.update();
+  p.draw();
+}
+
+class Particle {
+  constructor() {
+    //Position
+    //Width and height provided by P5
+    this.pos = createVector(random(width), random(height));
+    //Velocity
+    this.vel = createVector(random(-2, 2), random(-2, 2));
+    //Size
+    this.size = 10;
   }
 
-  circle(150, 150, 80);
+  update() {
+    this.pos.add(this.vel);
+  }
+
+  draw() {
+    noStroke();
+    fill("rgba(255, 255, 255, 0.5)");
+    circle(this.pos.x, this.pos.y, this.size * 2);
+  }
 }
